a7dc55c662d918c188c7099aad4ce073e20d21de
Hajin_Kim <kimhajin@yonsei.ac.kr>
2023-08-10 19:41:23 +0900

    :memo: Comment Karlo API reference

--- src/main/kotlin/ybigta/emoticon/ybigtaemoticonbackend/infra/karloapi/KarloApiClient.kt
+++ src/main/kotlin/ybigta/emoticon/ybigtaemoticonbackend/infra/karloapi/KarloApiClient.kt
@@ -3,6 +3,9 @@ package ybigta.emoticon.ybigtaemoticonbackend.infra.karloapi
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.PostMapping
 
+/**
+ * @see <a href="https://developers.kakao.com/docs/latest/ko/karlo/common">Karlo API</a>
+ */
 @FeignClient(
     "KarloApiClient",
     url = "https://api.kakaobrain.com",
